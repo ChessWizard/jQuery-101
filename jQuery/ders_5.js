f0e77dab935ad2fb78c67d7c04a2662864eb6794
ChessWizard <chesswizard2002@gmail.com>
2022-11-27 16:43:36 +0300

    some-filing-changes

--- jQuery/ders_5.js
+++ jQuery/ders_5.js
@@ -1,1 +1,19 @@
-// JavaScript source code
+$(function () {
+    // hem tiklandiginda hem de mouse uzerinden cekilince ayni islem yapilsin
+    // bu durumda eventler yan yana yazilarak ayni fonksiyon uzerinden yonetilirler
+    $("#p-1").on("click mouseleave", function () {
+        $(this).css("background-color", "red").css("height", "100px").css("width", "100px");
+    });
+
+    $("#p-2").on("click", function () {
+        $(this).css("background-color", "red").css("height", "100px").css("width", "100px");
+    })
+
+    // butona tiklandiginda paragrafin click ozelligi kapatilsin
+    $("#disableBtn").click(function () {
+        $("#p-2").off("click");
+    });
+
+    // change() ve select() metotlari
+
+});
