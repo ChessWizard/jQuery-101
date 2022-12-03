d41e908f974d1dcfcc0ed17a6b512a41123d3666
ChessWizard <chesswizard2002@gmail.com>
2022-12-04 02:49:07 +0300

    lessom-8-9-10-11-12 added

---
+++ jQuery/ders_10.js
@@ -0,0 +1,105 @@
+﻿$(function () {
+
+    // hideBtn id'li butona tiklaninca paragraflar kaybolsun
+    $("#hideBtn").click(function () {
+        $("p").hide();
+    });
+
+    // showBtn id'li butona tiklaninca ise tekrar gorunur olsunlar
+    $("#showBtn").click(function () {
+        $("p").show();
+    });
+
+    // Yavasca gizleme ve gosterme
+    $("#hideSlowlyBtn").click(function () {
+        $(".exampleP").hide("slow");
+    });
+
+    $("#showSlowlyBtn").click(function () {
+        $(".exampleP").show("slow");
+    });
+
+    // Hizlica gizleme ve gosterme
+    $("#hideFastBtn").click(function () {
+        $(".exampleP").hide("fast");
+    });
+
+    $("#showFastBtn").click(function () {
+        $(".exampleP").show("fast");
+    });
+
+    // milisaniye cinsinden gizleme ve gosterme suresi ayarlama
+    $("#hideSecondBtn").click(function () {
+        // 3 saniye icerisinde gizleme islemi gerceklesecek
+        $(".exampleP").hide(3000);
+    });
+
+    $("#showSecondBtn").click(function () {
+        // 5 saniye icerisinde gosterme islemi gerceklesecek
+        $(".exampleP").show(5000);
+    });
+
+    // Tek eventte acma ve kapama islemi -> toggle() metodu
+    // Gosterme ve gizleme islemleri ayni event uzerinden yonetilebilir bu sayede
+    $("#toggleBtn").click(function () {
+        $(".exampleP").toggle();
+    });
+
+    // Youtube yorumlar kismi yapimi
+
+    // Emoji picker implementasyonu
+    //Sonrasinda buraya bak!
+    //tinymce.init({
+    //    selector: "#commentArea",
+    //    onclick:"commentClick",
+    //    plugins: "emoticons autoresize",
+    //    toolbar: "emoticons",
+    //    toolbar_location: "bottom",
+    //    menubar: false,
+    //    statusbar: false
+    //});
+
+    // Oncelikle basta gozukmemesi gereken div gizli isaretlenmeli
+    var hidingElement = $(".hide");
+    hidingElement.hide();
+    var disabledElement = $("#doCommentBtn");
+    // disabled property'si true olsun
+    disabledElement.prop("disabled", true);
+
+    // Ancak yorum yapmak icin tiklandiginda gorunur olacak bu div icerisindeki butonlar
+    $("#commentArea").click(function () {
+        hidingElement.show();
+
+    });
+    
+    // Yazilma islemi sirasinda kontrol edilmesi gereken islemler
+    // Yalnizca tuslara tiklandigindaki durum kontrol edilirse basarili sonuc alamayiz
+    // Tusa basilip cekildigi durumlar icin bir kontrol yapilmalidir
+    $("#commentArea").keyup(function() {
+
+        // yorum yapma kismindaki deger aliniyor
+        var text = $("#commentArea").val();
+         // Eger herhangi bir sey yazilmamissa yorum yap butonu disable olmalidir
+        if (text == '') {
+            // disabled property'si true olsun
+            disabledElement.prop("disabled", true);
+        }
+        else {
+            // Eger text girildiyse disabled olmasin
+            disabledElement.prop("disabled", false);
+        }
+    });
+
+    // Eger iptal butonuna tiklanirsa satir tekrar gorunmez olsun
+    $("#cancelBtn").click(function () {
+        hidingElement.hide();
+    });
+
+    // Eger emoji butonuna tiklanirsa da bir emoji yazsin ekrana
+    $("#emojiBtn").click(function () {
+        // append metodu text ve html veriler eklemeye yarar element icerisine
+        // emojiler de yalnizca html formatta hex tipinde gorunur olduklari icin bu metodu kullandim
+        $("#commentArea").append("&#128536;");
+    });
+
+});
--- jQuery/ders_8.js
+++ jQuery/ders_8.js
@@ -1,3 +1,76 @@
 $(function () {
 
+    // Tikladigimiz DOM uyesiyle islem yapip yapmadigimizin kontrolu
+    // yalnizca bu event icin durumlari kontrol etmek adina parametre olarak event geceriz
+    $("h1, h2").click(function (event) {
+        // Eger tikladigimiz mevcut element h1 veya h2 ise
+        // this keywordu ile ulasiriz o anki secebildiklerimize -> h1 ve h2 secilebilir
+
+        // secilen elementin tag tipi h1 ise bir alert cikartilsin
+        if (event.currentTarget.tagName.toLowerCase() == "h1") {
+            alert("Birincil duzeyde baslik etiketi secildi!");
+        }
+
+        // HTML'de tag isimleri buyuk harflerle tutulur.
+        // bundan dolayi kucuk harfe cevirerek kontrolunu sagladim
+        if (event.currentTarget.tagName.toLowerCase() == "h2") {
+            alert("Ikincil duzeyde baslik etiketi secildi!");
+        }
+    });
+
+    // Bir event isletilirken gelen veriye ulasmak
+
+    // each() metoduyla tum p etiketleri geziliyor -> foreach loop mantigi
+    // buradaki i parametresi hangi p'nin secildigine dair indis degeri tutar
+    // 0. p, 1.p ... 
+    $("p").each(function (i) {
+        // Paragrafin kendisine tiklandiginda event parametresinin ulasabilecegi bir x property'si yaratildi
+        // x:i denilerek de kullanilacak fonksiyon parametresi icin property yaratildi -> tekrar bak
+        $(this).on("click", {x:i}, function (event) {
+            // secilmis olan paragrafin index degeri ve kacinci paragrafin kacinci verisi oldugu alinir
+            alert($(this).index()+1 + ". veri| " + (event.data.x + 1) + " paragraf");
+        });
+    });
+
+    // custom bir event yazilip ona bir isim verdigimizde
+    // verdigimiz isme ulasma
+    // function icerisine parametre olarak event verdigimizde
+    // ekledigimiz custom event icerisinde eventimizin property'lerine erisebilir duruma geliriz
+    $("div").on("changeParagraphColors", function (event) {
+        // Secilen paragrafin rengini degistir
+        $(this).css("color", "red");
+        // degistirme isleminden sonra buna sebep olan eventin ismini yazdir
+        alert(event.namespace);
+    })
+
+    // custom eventleri trigger ile tetikleyerek aktiflestiririz
+    // eventin ismini algilayabilmesi icin fonksiyona parametre olarak event burada da verilmelidir
+    $("div").click(function (event) {
+        // trigger edilecek eventin ismi yazilarak aktiflestirilir
+        $(this).trigger("changeParagraphColors");
+    });
+
+    // Belgede bulundugumuz koordinatlari almak
+    // Tum belge uzerinde mouse'u hareket ettirerek eszamanli olarak fare koordinatlarini gorelim
+    // event ozelliklerini kullanabilmemiz icin fonksiyonumuza yine parametre olarak geciyoruz
+    // bu event parametresi, bulundugumuz evente yani mousemove'a isaret eder
+    $(document).mousemove(function (event) {
+        $("#coordinates").text("x: " + event.pageX + " | " + "y: " + event.pageX);
+    });
+
+    // bulundugumuz eventin belirttigimiz event tipini etkisiz kilmak
+    // link olarak youtube ise tiklanabilirligi devam etsin
+    // twitch ise tiklanma ozelligi engellensin linkin
+    $("a").click(function (event) {
+
+        var currentLink = $(this).attr("href");
+
+        // eger link twitch iceriyorsa tiklanamaz olsun, gidilemesin
+        if (currentLink.includes("twitch")) {
+            // anchor tag icin tiklanma eventi etkisiz hale getirildi
+            event.preventDefault();
+            alert("Enough twitch today! Spend some time outside!");
+        }
+    });
+
 });
